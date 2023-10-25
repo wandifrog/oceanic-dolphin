c9bca65d8cc41d518c9c183120a11aa51abd56d4
Wandi <wandifrog@gmail.com>
2023-10-25 22:15:59 +0700

    chore: update App.hook.ts

--- src/hooks/App.hook.ts
+++ src/hooks/App.hook.ts
@@ -1,6 +1,19 @@
 import { AppDispatchContext, AppStateContext } from 'contexts/App.context';
 import { useContext } from 'react';
 
+/**
+ * Custom React hook for accessing App Context.
+ *
+ * This hook is a convenience function that combines the `useAppState` and
+ * `useAppDispatch` hooks to provide both the application state and dispatch
+ * function in a single call.
+ *
+ * @returns {[contextState, dispatchContextState]} An array containing the application state and dispatch function.
+ */
+function useApp() {
+  return [useAppState(), useAppDispatch()];
+}
+
 function useAppState() {
   const context = useContext(AppStateContext);
   if (context === undefined) throw new Error('useAppState must be used within a AppProvider');
@@ -11,10 +24,6 @@ function useAppDispatch() {
   const context = useContext(AppDispatchContext);
   if (context === undefined) throw new Error('useAppDispatch must be used within a AppProvider');
   return context;
-}
-
-function useApp(): [AppState, AppDispatch] {
-  return [useAppState(), useAppDispatch()];
 }
 
 export default useApp;
